65b5eaf7a188faafcda3eb7e8472859a11148bbd
Anthony Almonte <worldrdanthony@gmail.com>
2025-06-10 19:34:53 -0400

    Inventory.tsx on the way

---
+++ src/features/products/inventory/pages/Inventory.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Inventory: React.FC = () => {
+  return (
+    <div className=''>
+      <div>
+        <h2 className="text-black dark:text-white text-2xl font-semibold py-2">Inventario</h2>
+      </div>
+    </div>
+  );
+};
